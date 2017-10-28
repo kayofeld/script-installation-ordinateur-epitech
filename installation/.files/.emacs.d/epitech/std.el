9307b1aae4cd6fe82a8852b627d2f043b7acaa78
Hamel Gabriel <gabi6329@gmail.com>
2017-10-28 23:53:40 +0200

    Update std.el

--- installation/.files/.emacs.d/epitech/std.el
+++ installation/.files/.emacs.d/epitech/std.el
@@ -1,21 +1,21 @@
 ;;
-;; div.el for emacs conf in site-local
-;; 
-;; Made by Nicolas Sadirac
-;; Login   <rn@epita.fr>
-;; 
-;; Started on  Sun Oct 17 02:20:57 1993 Nicolas Sadirac
-;; Last update Thu Jul  8 12:47:38 2010 root
+;; EPITECH PROJECT, 2017
+;; emacs configuration
+;; File description:
+;; global emacs configuration to help students
 ;;
 
-(defun do_insert_time () 
-  (interactive)
- (insert-string (current-time-string)))
 (set-variable 'c-argdecl-indent   0)
-
 
 (normal-erase-is-backspace-mode 0)
 
+; Define keyboard shortcuts
 (global-set-key "" 'backward-delete-char)
 (global-set-key "" 'compile)
 (global-set-key "" 'goto-line)
+
+; Adjust indentation settings
+(setq c-default-style "linux"
+      c-basic-offset 8
+      tab-width 8
+      indent-tabs-mode t)
